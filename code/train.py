46e09eb1462f48d7bf01c237e96c22cebafbfffa
sohn <59533593+oikosohn@users.noreply.github.com>
2022-03-18 17:18:56 +0900

    Update train.py

--- code/train.py
+++ code/train.py
@@ -83,9 +83,9 @@ if __name__ == "__main__":
     parser.add_argument("--num_labels", type=int, default=1)
     parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--num_train_epochs", type=int, default=10)
-    parser.add_argument("--batch_size", type=int, default=32)
+    parser.add_argument("--batch_size", type=int, default=64)
     parser.add_argument("--learning_rate", type=float, default=5e-5)
-    parser.add_argument("--gradient_accumulation_steps", type=int, default=2)
+    parser.add_argument("--gradient_accumulation_steps", type=int, default=1)
     parser.add_argument("--weight_decay", type=float, default=0.01)
     parser.add_argument("--max_seq_length", type=int, default=110)
 
